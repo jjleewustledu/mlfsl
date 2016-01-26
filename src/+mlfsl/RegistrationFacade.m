bb7b38005973977ff91a5992537da005b1376935
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2016-01-26 01:15:22 -0600

    Checking in prior to converting many Alignment classes to Registration classes.   Nomenclature change helps preserve  legacy code base.

--- src/+mlfsl/RegistrationFacade.m
+++ src/+mlfsl/RegistrationFacade.m
@@ -10,8 +10,15 @@ classdef RegistrationFacade < handle
  	
     properties (Dependent)
         sessionData
+        
         talairach
-        petAtlas
+        pet
+        fdg
+        gluc
+        ho
+        oo
+        oc
+        tr
     end
     
     methods %% GET
@@ -24,16 +31,49 @@ classdef RegistrationFacade < handle
             end
             g = this.talairach_;
         end
-        function g = get.petAtlas(this)
-            if (isempty(this.petAtlas_))
-                ic = mlfourd.ImagingContext(this.sessionData.pet_fqfns);
-                this.petAtlas_ = ic.atlas;
+        function g = get.pet(this)
+            g = mlfourd.ImagingContext( ...
+                { this.fdg this.gluc this.ho this.oo this.oc this.tr });
+        end
+        function g = get.fdg(this)
+            if (isempty(this.fdg_))
+                this.fdg_ = mlfourd.ImagingContext(this.sessionData_.fdg_fqfn);
             end
-            g = this.petAtlas_;
+            g = this.fdg_;
+        end
+        function g = get.gluc(this)
+            if (isempty(this.gluc_))
+                this.gluc_ = mlfourd.ImagingContext(this.sessionData_.gluc_fqfn);
+            end
+            g = this.gluc_;
+        end
+        function g = get.ho(this)
+            if (isempty(this.ho_))
+                this.ho_ = mlfourd.ImagingContext(this.sessionData_.ho_fqfn);
+            end
+            g = this.ho_;
+        end
+        function g = get.oo(this)
+            if (isempty(this.oo_))
+                this.oo_ = mlfourd.ImagingContext(this.sessionData_.oo_fqfn);
+            end
+            g = this.oo_;
+        end
+        function g = get.oc(this)
+            if (isempty(this.oc_))
+                this.oc_ = mlfourd.ImagingContext(this.sessionData_.oc_fqfn);
+            end
+            g = this.oc_;
+        end
+        function g = get.tr(this)
+            if (isempty(this.tr_))
+                this.tr_ = mlfourd.ImagingContext(this.sessionData_.tr_fqfn);
+            end
+            g = this.tr_;
         end
     end
     
-	methods 		  
+	methods
  		function this = RegistrationFacade(varargin)
  			%% RegistrationFacade
             %  @param sessionData is an mlpipeline.SessionData specifying identifiers for the study session, including
@@ -42,17 +82,15 @@ classdef RegistrationFacade < handle
             %  @return this is a facade pattern for imaging alignment.
 
             ip = inputParser;
-            addParameter(ip, 'sessionData',         [], @(x) isa(x, mlpipeline.SessionData));
-            addParameter(ip, 'registrationBuilder', [], @(x) isa(x, mlfsl.RegistrationBuilder));
+            addParameter(ip, 'sessionData', [], @(x) isa(x, mlpipeline.SessionData));
             parse(ip, varargin{:});
             
-            this.sessionData_         = ip.Results.sessionData;
-            this.registrationBuilder_ = ip.Results.registrationBuilder;
+            this.sessionData_ = ip.Results.sessionData;
         end
         
         function this = applyTransform(this, varargin)
             ip = inputParser;
-            addParameter(ip, 'interpolation', 'trilinear', @isinterpolation);
+            addParameter(ip, 'interpolation', 'trilinear', @this.isinterpolation);
             parse(ip, varargin{:});
         end
         function this = motionCorrectPet(this, varargin)
@@ -61,38 +99,53 @@ classdef RegistrationFacade < handle
             addParameter(ip, 'indirectBlurring', [], @isnumeric);
             parse(ip, varargin{:});
         end
-        function prod = registerTalairachOnPetAtlas(this, varargin)
-            %% ALIGNTALAIRACHONPET
-            %  @param angles
-            %  @param cost
-            %  @return prod is a struct with fields:  talairach, petAtlas, talairachOnPetAtlas, xfm, inverseXfm.
-            %  xfm transforms tailairch to petAtlas.  The first three are mlfourd.ImagingContext and 
-            %  the latter are fully-qualified filenames.
+        function prod = registerTalairachWithPet(this)
+            %% REGISTERTALAIRACHWITHPET
+            %  @return prod is a struct with products as fields.
             
-            ip = inputParser;
-            addParameter(ip, 'angles', [-90 90], @isnumeric); % isotropic
-            addParameter(ip, 'cost', 'normmi', @iscost);
-            parse(ip, varargin{:});            
+            import mlfsl.*;
+            msrb = MultispectralRegistrationBuilder(this.sessionData);
+            prb  = PetRegistrationBuilder(this.sessionData);
             
-            bldr = mlfsl.MultispectralRegistrationBuilder(this.sessionData);
-            bldr = bldr.registerLinear(this.talairach, this.petAtlas);            
-            bldr = bldr.applyTransform(this.talairach, this.petAtlas);
+            prod.talairach = this.talairach;
+            prod.petAtlas  = this.pet.atlas;
+            prod.fdg       = prb.motionCorrect(this.fdg);
+            prod.gluc      = prb.motionCorrect(this.gluc);
+            prod.ho        = prb.motionCorrect(this.ho);
+            prod.oo        = prb.motionCorrect(this.oo);
+            prod.oc        = this.oc;
+            prod.tr        = this.tr;
             
-            prod.talairach           = this.talairach;
-            prod.petAtlas            = this.petAtlas;
-            prod.talairachOnPetAtlas = bldr.product;
-            prod.xfm                 = bldr.xfm;
-            prod.inverseXfm          = bldr.inverseXfm;
+            tail_on_atl = msrb.registerSurjective(prod.talairach, prod.petAtlas);
+            
+             fdg_on_atl = prb.registerBijective(prod.fdg,  prod.petAtlas);
+            gluc_on_atl = prb.registerBijective(prod.gluc, prod.petAtlas);
+              ho_on_atl = prb.registerBijective(prod.ho,   prod.petAtlas);
+              oo_on_atl = prb.registerBijective(prod.oo,   prod.petAtlas);
+              oc_on_atl = prb.registerBijective(prod.oc,   prod.petAtlas);
+              tr_on_atl = prb.registerBijective(prod.tr,   prod.petAtlas);
+            
+            prod.talairach_on_fdg  = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed( fdg_on_atl));
+            prod.talairach_on_gluc = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed(gluc_on_atl));
+            prod.talairach_on_ho   = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed(  ho_on_atl));
+            prod.talairach_on_oo   = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed(  oo_on_atl));
+            prod.talairach_on_oc   = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed(  oc_on_atl));
+            prod.talairach_on_tr   = msrb.registerComposed(tail_on_atl, msrb.inverseTransformed(  tr_on_atl));
         end
     end 
     
     %% PRIVATE
     
     properties (Access = private)
-        sessionData_
-        registrationBuilder_
+        sessionData_        
+        
         talairach_
-        petAtlas_
+        fdg_
+        gluc_
+        ho_
+        oo_
+        oc_
+        tr_
     end
     
     methods (Static, Access = private) % TO DO:  move to builder classes
